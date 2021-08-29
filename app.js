e5ae0b30d3b953f4cd186e5208f633d4a5804b9c
jianylei <jianyonglei@gmail.com>
2021-08-29 18:36:03 -0400

    keyboard integration + seperated eventlisteners into individual functions

--- app.js
+++ app.js
@@ -9,68 +9,6 @@ const equalBtn = document.querySelector(".btn-equal");
 const prevOperation = document.querySelector(".previous-operation");
 const currOperation = document.querySelector(".current-operation");
 
-clearBtn.onclick = clearScreen;
-
-deleteBtn.onclick = () => {
-    if(currOperation.innerText) {
-        let tmp = currOperation.innerText;
-        currOperation.innerText = tmp.substring(0, tmp.length - 1);
-    }
-    rhs = currOperation.innerText;
-}
-
-equalBtn.onclick = () => {
-    if(prevOperation.innerText && operator && lhs && rhs) {
-        let tmp = operate();
-        if(tmp) {
-            prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
-            currOperation.innerText = tmp;
-        }
-    }
-    
-}
-
-operatorBtn.forEach(button => {
-    button.onclick = () => {
-        if(!errflag) {
-        
-
-        if(operator && lhs && rhs ){
-            let tmp = operate();
-            prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
-            currOperation.innerText = tmp;
-        }
-
-        if(!currOperation.innerText) {
-            prevOperation.innerText = "0 " + button.innerText;
-        }
-        else {
-            prevOperation.innerText = currOperation.innerText + " " + button.innerText;
-        }
-
-        lhs = currOperation.innerText;
-        operator = button.innerText;
-        calcflag = true;
-    }}
-})
-
-numBtn.forEach(button => {
-    button.onclick = () => {
-        if(errflag) {
-            clearScreen();
-            errflag = false;
-        }
-        if(!currOperation.innerText  || calcflag) {
-            currOperation.innerText = button.innerText;
-            calcflag = false;
-        }
-        else {
-            append(button.innerText)
-        }
-        rhs = currOperation.innerText;
-    }
-})
-
 function clearScreen() {
     prevOperation.innerText = "";
     currOperation.innerText = "";
@@ -80,9 +18,61 @@ function clearScreen() {
 }
 
 function append(num){
-    let tmp = currOperation.innerText
-    if(num !== "." || !tmp.includes(".")) {
-        currOperation.innerText = tmp + num;
+    if (errflag) {
+        clearScreen();
+        errflag = false;
+    }
+    if (!currOperation.innerText || calcflag) {
+        currOperation.innerText = num;
+        calcflag = false;
+    }
+    else {
+        let tmp = currOperation.innerText
+        if (num !== "." || !tmp.includes(".")) {
+            currOperation.innerText = tmp + num;
+        }
+    }
+    rhs = currOperation.innerText;
+}
+
+function deleteKey() {
+    if(currOperation.innerText) {
+        let tmp = currOperation.innerText;
+        currOperation.innerText = tmp.substring(0, tmp.length - 1);
+    }
+    rhs = currOperation.innerText;
+}
+
+function setOperator(_operator){
+    if(!errflag) {
+        
+        if(operator && lhs && rhs ){
+            let tmp = operate();
+            prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
+            currOperation.innerText = tmp;
+        }
+
+        if(!currOperation.innerText) {
+            prevOperation.innerText = "0 " + _operator;
+        }
+        else {
+            prevOperation.innerText = currOperation.innerText + " " + _operator;
+        }
+
+        lhs = currOperation.innerText;
+        operator = _operator;
+        calcflag = true;
+    }
+}
+
+function compute() {
+    if(prevOperation.innerText && operator && lhs && rhs) {
+        let tmp = operate();
+        if(tmp) {
+            prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
+            currOperation.innerText = tmp;
+            calcflag = true;
+        }
     }
 }
 
@@ -113,3 +103,48 @@ function operate(){
         operator = "";
         return tmp;
 }
+
+//event listeners
+clearBtn.onclick = clearScreen;
+
+deleteBtn.onclick = deleteKey;
+
+equalBtn.onclick = compute;
+
+numBtn.forEach(button => {
+    button.onclick = () => append(button.innerText);
+})
+
+operatorBtn.forEach(button => {
+    button.onclick = () => setOperator(button.innerText);
+})
+
+//keyboard integration
+window.onkeydown = (e) => {
+    if(e.key >= 0 && e.key <= 9 || e.key === ".") {
+        append(e.key);
+    }
+    if(e.key === "=" || e.key === "Enter") {
+        compute();
+    }
+    if(e.key === "Backspace") {
+        deleteKey();
+    }
+    if(e.key === "Escape") {
+        clearScreen();
+    }
+    if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === "x") {
+        let tmp;
+        switch(e.key) {
+            case "/":
+                tmp = "÷"
+                break;
+            case "*":
+                tmp = "x"
+                break;
+            default:
+                tmp = e.key;
+        }
+        setOperator(tmp);
+    }
+  }
