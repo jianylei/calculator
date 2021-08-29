987168f6ed1b1c49cdb17ceb4a531bf5e615995a
jianylei <jianyonglei@gmail.com>
2021-08-29 17:33:16 -0400

    completed main calculator logic, operation functions tbd

--- app.js
+++ app.js
@@ -1,35 +1,92 @@
+let rhs, lhs, operator = "";
+let flag= false;
+
 const clearBtn = document.querySelector(".btn-clear");
 const deleteBtn = document.querySelector(".btn-delete");
 const numBtn = document.querySelectorAll(".btn-number");
-const prevOperarion = document.querySelector(".previous-operation");
-const currOperarion = document.querySelector(".current-operation");
+const operatorBtn = document.querySelectorAll(".btn-operator");
+const equalBtn = document.querySelector(".btn-equal");
+const prevOperation = document.querySelector(".previous-operation");
+const currOperation = document.querySelector(".current-operation");
 
 clearBtn.onclick = () => {
-    prevOperarion.innerText = "";
-    currOperarion.innerText = "";
+    prevOperation.innerText = "";
+    currOperation.innerText = "";
+    lsh = "";
+    rhs = "";
+    operator = "";
 }
 
 deleteBtn.onclick = () => {
-    if(currOperarion.innerText !== "") {
-        let tmp = currOperarion.innerText;
-        currOperarion.innerText = tmp.substring(0, tmp.length - 1);
+    if(currOperation.innerText !== "") {
+        let tmp = currOperation.innerText;
+        currOperation.innerText = tmp.substring(0, tmp.length - 1);
     }
+    rhs = currOperation.innerText;
 }
+
+equalBtn.onclick = () => {
+    if(prevOperation.innerText !== "" && operator !== "" && lhs && rhs) {
+        let tmp = operate();
+        prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
+        currOperation.innerText = tmp;
+    }
+    
+}
+
+operatorBtn.forEach(button => {
+    button.onclick = () => {
+        
+        if(operator !== "" && lhs && rhs){
+            let tmp = operate();
+            prevOperation.innerText = prevOperation.innerText + " " + currOperation.innerText + " =";
+            currOperation.innerText = tmp;
+        }
+        lhs = currOperation.innerText;
+        operator = button.innerText;
+        flag = true;
+        console.log("lhs :"+lhs+", op: " +operator+", rhs: "+rhs);
+
+        if(currOperation.innerText === "") {
+            prevOperation.innerText = "0 " + button.innerText;
+        }
+        else {
+            prevOperation.innerText = currOperation.innerText + " " + button.innerText;
+        }
+    }
+})
 
 numBtn.forEach(button => {
     button.onclick = () => {
-        if(currOperarion.innerText === "") {
-            currOperarion.innerText = button.innerText;
+        if(currOperation.innerText === ""  || flag) {
+            currOperation.innerText = button.innerText;
+            flag = false;
         }
         else {
             append(button.innerText)
         }
+        rhs = currOperation.innerText; console.log("lhs :"+lhs+", op: " +operator+", rhs: "+rhs);
     }
-});
+})
 
 function append(num){
-    let tmp = currOperarion.innerText
+    let tmp = currOperation.innerText
     if(num !== "." || !tmp.includes(".")) {
-        currOperarion.innerText = tmp + num;
+        currOperation.innerText = tmp + num;
+    }
+}
+
+function operate(){
+    if(lhs, rhs, operator) {
+        let tmp;
+        switch (operator) {
+            case "x":
+                tmp = parseFloat(lhs) * parseFloat(rhs);
+                break;
+        }console.log("lhs :"+lhs+", op: " +operator+", rhs: "+rhs);
+        lhs = tmp;
+        rhs = "";
+        operator = "";
+        return tmp;
     }
 }
